98f2b0a4b13ef7e478478fee811c1aab5321faa3
ldg <ldg10202@naver.com>
2016-12-26 18:17:48 +0900

    hello world

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/',function (req,res) {
+  res.send('Hello World!!!!');
+});
+
+app.listen(5000,function(){
+  console.log('Serber On!!!');
+});
