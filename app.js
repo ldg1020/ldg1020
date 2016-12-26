874e1dbfd798f349972120162f4ad809db719bc8
ldg <ldg10202@naver.com>
2016-12-26 18:45:58 +0900

    index CSS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function (req,res) {
-  res.send('Hello World!!!!');
-});
+app.use(express.static(path.join(__dirname + '/public')));
+//console.log(__dirname);
+//app.get('/',function (req,res) {
+//  res.send('Hello World!!!!');
+//});
 
 app.listen(5000,function(){
-  console.log('Serber On!!!');
+  console.log('Server On!!!');
 });
